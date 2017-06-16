a6a68b81d1e0d84e0df87ecfe55260133fac0934
阿部遥人 <abeharu0713@gmail.com>
2017-06-16 19:23:03 +0900

    Initial Commit

---
+++ Kikkake/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kikkake
+//
+//  Created by 阿部遥人 on 2017/06/16.
+//  Copyright © 2017年 阿部遥人. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

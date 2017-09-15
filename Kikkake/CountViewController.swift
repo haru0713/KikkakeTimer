64c94d5b3d8a108299b8d9fe3399f96f19bb3b4c
阿部遥人 <abeharu0713@gmail.com>
2017-09-15 18:33:55 +0900

    よろ

--- Kikkake/CountViewController.swift
+++ Kikkake/CountViewController.swift
@@ -7,29 +7,79 @@
 //
 
 import UIKit
+import UserNotifications
 
 class CountViewController: UIViewController {
-
+    
+    var Chunnumber:Int = 0
+    var Cnumber:Int = 0
+    var timer = Timer()
+    @IBOutlet var Chunlabel:UILabel!
+    @IBOutlet var Clabel:UILabel!
+    
+    func up(){
+        if Cnumber == 0 && Chunnumber >= 1{
+            Chunnumber = Chunnumber - 1
+            Cnumber = Cnumber + 60
+        }else{
+            
+        }
+       
+        if Cnumber >= 1{
+            Cnumber = Cnumber - 1
+        }else{
+            
+            
+        }
+    
+        
+        Chunlabel.text = String(Chunnumber)
+        Clabel.text = String(Cnumber)
+        
+        if Cnumber == 0 && Chunnumber == 0{
+            timer.invalidate()
+            
+            let storyboard: UIStoryboard = self.storyboard!
+            let nextView = storyboard.instantiateViewController(withIdentifier: "Title") as! ViewController
+            self.present(nextView, animated: true, completion: nil)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+    
 
-        // Do any additional setup after loading the view.
+        Chunlabel.text = String(Chunnumber)
+        Clabel.text = String(Cnumber)
+        
+        let timer = Timer.scheduledTimer(timeInterval:1,
+                                         target: self,
+                                        selector:#selector(self.up),
+                                        userInfo: nil,
+                                        repeats: true)
+        
+                                        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func cansell(){
+        timer.invalidate()
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
     }
-    */
-
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
--- Kikkake/ViewController.swift
+++ Kikkake/ViewController.swift
@@ -7,19 +7,107 @@
 //
 
 import UIKit
+import UserNotifications
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+ 
+    var number:Int = 0
+    var hunnumber:Int = 1
+    @IBOutlet var picker: UIPickerView!
+    // 選択肢
+    let dataList = ["iOS", "macOS", "tvOS", "Android", "Windows"]
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "toCount") {
+            let countView : CountViewController = segue.destination as! CountViewController
+            countView.Chunnumber = self.hunnumber
+            countView.Cnumber = self.number
+            
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+       
+        // プロトコルの設定
+        picker.delegate = self
+        picker.dataSource = self
+        
+        // はじめに表示する項目を指定
+        picker.selectRow(1, inComponent: 0, animated: true)
+        
+        
+        // 画面にピッカーを追加
+        self.view.addSubview(picker)
+    }
+    
+    // UIPickerViewDataSource
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        // 表示する列数
+        return 2
     }
 
+    //アイテムの個数を返す
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if component == 0 {
+            return 31
+
+        }else{
+        return 61
+  
+        }}
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        // 表示する文字列を返す
+        return String(row)
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+       // 選択時の処理
+        if component  == 0{
+            hunnumber = row
+        }else{
+            number = row
+        }
+    }
+    
+  
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func set(){
+        let trigger: UNNotificationTrigger = UNTimeIntervalNotificationTrigger(timeInterval:(TimeInterval(hunnumber*60+number)), repeats: false)
+        
+        let trigger2: UNNotificationTrigger = UNTimeIntervalNotificationTrigger(timeInterval:(TimeInterval(hunnumber*60+number+10)), repeats: false)
+
+       
+        
+        //表示の設定
+        let content = UNMutableNotificationContent()
+        content.title = "タイトルです"
+        content.body = "ボディです"
+        content.sound = UNNotificationSound.default()
+        
+        // デフォルトの通知。画像などは設定しない
+        let request = UNNotificationRequest(identifier: "normal",
+                                            content: content,
+                                            trigger: trigger)
+        
+        let request2 = UNNotificationRequest(identifier: "normal",
+                                            content: content,
+                                            trigger: trigger2)
+        
+        
+        //通知を予約
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+        UNUserNotificationCenter.current().add(request2, withCompletionHandler: nil)
+    
+
+    }
+
 
 }
-
